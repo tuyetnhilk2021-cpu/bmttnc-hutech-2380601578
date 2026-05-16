f765e50e0f6419c9b88393b89ded1fb2b7ba7cf2
tuyetnhilk2021-cpu <tuyetnhilk2021@gmail.com>
2026-05-16 22:15:53 +0700

    Fix variable name for input string in ex03_05.py

--- LAB-01/ex03/ex03_05.py
+++ LAB-01/ex03/ex03_05.py
@@ -8,6 +8,6 @@ def dem_so_lan_xuat_hien(lst):
     return count_dict
 
 input_string = input("Nhập danh sách các từ, cách nhau bằng dấu cách: ")
-word_list = input_list.split()
+word_list = input_string.split()
 so_lan_xuat_hien = dem_so_lan_xuat_hien(word_list)
 print("Số lần xuất hiện của các phần tử: ", so_lan_xuat_hien)
