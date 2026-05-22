d20de7baf0150b231bf9b9c66b2570897420637d
tuyetnhilk2021 <tuyetnhilk2021@gmail.com>
2026-05-22 21:02:31 +0700

    [add] LAB-01 ex04 and .gitignore

---
+++ LAB-01/ex04/QuanLySinhVien.py
@@ -0,0 +1,125 @@
+from SinhVien import SinhVien
+
+class QuanLySinhVien:
+    listSinhVien = []
+    
+    def generateID(self):
+        maxId = 1
+        if(self.soLuongSinhVien() > 0):
+            maxId = self.listSinhVien[0]._id
+            for sv in self.listSinhVien:
+                if (maxId < sv._id):
+                    maxId = sv._id
+            maxId = maxId + 1
+        return maxId
+    
+    def soLuongSinhVien(self):
+        return self.listSinhVien.__len__()
+    
+    def nhapSinhVien(self):
+        svId = self.generateID()
+        name = input("Nhập tên sinh viên: ")
+        sex = input("Nhập giới tính sinh viên: ")
+        major = input("Nhập chuyên ngành của sinh viên: ")
+        diemTB = float(input("Nhập điểm của sinh viên: "))
+        sv = SinhVien(svId,name,sex,major,diemTB)
+        self.XepLoaiHocLuc(sv)
+        self.listSinhVien.append(sv)  
+
+    def updateSinhVien(self, ID):
+        sv:SinhVien = self.findByID(ID)
+        if(sv != None):
+            name = input("Nhập tên sinh viên: ")
+            sex = input("Nhập giới tính sinh viên: ")
+            major = input("Nhập chuyên ngành của sinh viên: ")
+            diemTB = float(input("Nhập điểm của sinh viên: "))
+            sv.name = name
+            sv.sex = sex
+            sv.major = major
+            sv.diemTB = diemTB
+            self.XepLoaiHocLuc(sv)
+        else:
+            print("Sinh vien co ID = {} khong ton tai.".format(ID))
+            
+    def sortByID(self):
+        self.listSinhVien.sort(key=lambda x: x._id, reverse=False)
+        
+    def sortByName(self):
+        self.listSinhVienV.sort(key=lambda x: x._name, reverse=False)
+        
+    def sortByDiemTB(self):
+        self.listSinhVien.sort(key=lambda x: x._diemTB, reverse=False)
+        
+    def findByID(self, ID):
+        searchResult = None
+        if (self.soLuongSinhVien() > 0):
+            for sv in self.listSinhVien:
+                if (sv._id == ID):
+                    searchResult = sv
+        return searchResult
+    
+    def findByName(self, keyword):
+        listSV = []
+        if (self.soLuongSinhVien() > 0):
+            for sv in self.listSinhVien:
+                if (keyword.upper() in sv._name.upper()):
+                    listSV.append(sv)
+        return listSV
+    
+    def deleteById(self, ID):
+        isDeleted = False
+        sv = self.findByID(ID)
+        if (sv != None):
+            self.listSinhVien.remove(sv)
+            isDeleted = True
+        return isDeleted
+    
+    def XepLoaiHocLuc(self, sv:SinhVien):
+        if(sv._diemTB >= 8):
+            sv._hocLuc = "Gioi"
+        elif(sv._diemTB >= 6.5):
+            sv._hocLuc = "Kha"
+        elif(sv._diemTB >= 5):
+            sv._hocLuc = "Trung Binh"
+        else:
+            sv.hocLuc = "Yeu"
+            
+    def ShowSinhVien(self,ListSV):
+        print("{:<8} {:<18} {:<8} {:<15} {:<8} {:<8}"
+              .format("ID","Name","Set","Major","Điểm TB","Học Lực"))
+        if(ListSV.__len__() > 0):
+            for sv in ListSV:
+                print("{:<8} {:<18} {:<8} {:<15} {:<8} {:<8}"
+                      .format(sv._id,sv._name,sv._sex,sv._major,sv._diemTB,sv._hocLuc))
+        print("\n")
+        
+    def getListSinhVien(self):
+        return self.listSinhVien
+    
+    
+      
+
+
+    def xoaSinhVien(self,ID):
+        for i in range(self.soLuongSinhVien()):
+            if(self.listSV[i].id == ID):
+                self.listSV.pop(i)
+                return True
+        return False
+    
+
+    def timSinhVienByName(self,name):
+        for i in range(self.soLuongSinhVien()):
+            if(self.listSV[i].name == name):
+                return self.listSV[i]
+        return None
+                
+
+    def timSinhVienByID(self,ID):
+        for i in range(self.soLuongSinhVien()):
+            if(self.listSV[i].id == ID):
+                return self.listSV[i]
+        return None
+    
+
+    
