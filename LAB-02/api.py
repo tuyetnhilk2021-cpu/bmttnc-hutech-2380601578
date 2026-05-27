686acf09e04758df386dd73ac65f7317dae032a0
tuyetnhilk2021 <tuyetnhilk2021@gmail.com>
2026-05-27 20:06:52 +0700

    [add] LAB-02 transposition

--- LAB-02/api.py
+++ LAB-02/api.py
@@ -3,6 +3,7 @@ from cipher.caesar import CaesarCipher
 from cipher.vigenere import VigenereCipher
 from cipher.railfence import RailFenceCipher
 from cipher.playfair import PlayFairCipher
+from cipher.transposition import TranspositionCipher
 app = Flask(__name__) 
 
 
@@ -85,6 +86,25 @@ def playfair_decrypt():
     key = data['key']
     playfair_matrix = playfair_cipher.create_playfair_matrix(key)
     decrypted_text = playfair_cipher.playfair_decrypt(cipher_text, playfair_matrix)
+    return jsonify({'decrypted_text': decrypted_text})
+
+#Transposition
+transposition_cipher = TranspositionCipher()
+
+@app.route('/api/transposition/encrypt', methods=['POST'])
+def transposition_encrypt():
+    data = request.get_json()
+    plain_text = data.get('plain_text')
+    key = int(data.get('key'))
+    encrypted_text = transposition_cipher.encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/transposition/decrypt', methods=['POST'])
+def transposition_decrypt():
+    data = request.get_json()
+    cipher_text = data.get('cipher_text')
+    key = int(data.get('key'))
+    decrypted_text = transposition_cipher.decrypt(cipher_text, key)
     return jsonify({'decrypted_text': decrypted_text})
 
 if __name__== "__main__":
